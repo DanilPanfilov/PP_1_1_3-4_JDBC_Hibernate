7d279e97bb9b52124e3989216bb353ab3b54b3fa
Danil <danil.pancfil@gmail.com>
2022-12-22 14:58:26 +0700

    perfect3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,7 +11,7 @@ public class Main {
         userService.saveUser("QQQQ", "qqqq", (byte) 12);
         userService.saveUser("wwww", "WWWW", (byte) 14);
         userService.saveUser("eeee", "EEEE", (byte) 15);
-        userService.saveUser("rrrr", "RRRR", (byte) 16);
+        userService.saveUser("rrrr", "RRRRR", (byte) 16);
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
