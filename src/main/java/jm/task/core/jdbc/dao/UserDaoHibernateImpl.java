215df907a8cf68deb106c6ef77c0849516aa8aea
Danil <danil.pancfil@gmail.com>
2022-12-24 18:24:13 +0700

    perfect4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -22,76 +23,77 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.createSQLQuery("  CREATE TABLE if NOT EXISTS `schema`.`users` (\n" +
-                        "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
-                        "  `name` VARCHAR(45) NOT NULL,\n" +
-                        "  `lastName` VARCHAR(45) NOT NULL,\n" +
-                        "  `age` INT NOT NULL,\n" +
-                        "                PRIMARY KEY (`id`))\n" +
-                        "        ENGINE = InnoDB\n" +
-                        "        DEFAULT CHARACTER SET = utf8;")
-                .executeUpdate();
-        transaction.commit();
-        session.close();
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("  CREATE TABLE if NOT EXISTS `schema`.`users` (\n" +
+                            "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                            "  `name` VARCHAR(45) NOT NULL,\n" +
+                            "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                            "  `age` INT NOT NULL,\n" +
+                            "                PRIMARY KEY (`id`))\n" +
+                            "        ENGINE = InnoDB\n" +
+                            "        DEFAULT CHARACTER SET = utf8;")
+                    .executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.createSQLQuery("DROP TABLE if EXISTS users;").executeUpdate();
-        transaction.commit();
-        session.close();
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE if EXISTS users;").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        User user = new User();
-        user.setName(name);
-        user.setLastName(lastName);
-        user.setAge(age);
-        session.save(user);
-        transaction.commit();
-        System.out.println("User с именем –" + name + " добавлен в базу данных");
-        session.close();
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.save(user);
+            transaction.commit();
+            System.out.println("User с именем –" + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        User user = new User();
-        user.setId(id);
-        session.delete(user);
-        transaction.commit();
-        session.close();
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = sf.openSession();
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaQuery cq = cb.createQuery(User.class);
-        Root<User> root = cq.from(User.class);
-        cq.select(root);
-
-        Query query = session.createQuery(cq);
-        List<User> list = query.getResultList();
-        session.close();
-        return list;
+        TypedQuery<User> query = sf.createEntityManager().createQuery("SELECT u FROM User u", User.class);
+        return query.getResultList();
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.createSQLQuery(" TRUNCATE TABLE users ;").executeUpdate();
-        transaction.commit();
-        session.close();
+        try (Session session = sf.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery(" TRUNCATE TABLE users ;").executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
